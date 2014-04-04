ee88aafb440c267759f02d3e6cc000074f2c3e2b
vasupol11 <vasupol11@yahoo.com>
2014-04-04 21:43:03 +0700

    first init
    The first initialization has background, coin, block, and obstacle. The
    coin has animation and block can move. Block is buggy.

---
+++ src/Coin.js
@@ -0,0 +1,18 @@
+var Coin = cc.Sprite.extend({
+    ctor: function() {
+        this._super();
+        this.initWithFile( 'Images/coin_1.png' );  
+        this.movingAction = this.createAnimationAction();
+        this.runAction( this.movingAction );
+    },
+
+     createAnimationAction: function() {
+	var animation = new cc.Animation.create();
+	animation.addSpriteFrameWithFile( 'Images/coin_1.png' );
+	animation.addSpriteFrameWithFile( 'Images/coin_2.png' );
+	animation.addSpriteFrameWithFile( 'Images/coin_3.png' );
+	console.log( animation.getDelayPerUnit() );
+	animation.setDelayPerUnit( 0.2 );
+	return cc.RepeatForever.create( cc.Animate.create( animation ) );
+    }
+});
